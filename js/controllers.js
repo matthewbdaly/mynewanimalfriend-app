de42f82b523ee9d6d8ed5059c7903cb708ab1fca
Matthew Daly <matthewbdaly@gmail.com>
2016-09-18 20:19:01 +0100

    Added first controller

---
+++ js/controllers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+require('angular');
+require('angular-route');
+require('./services');
+
+angular.module('mynewanimalfriend.controllers', [
+  'mynewanimalfriend.services',
+  "ngMaterial"
+])
+
+.controller('LoginCtrl', function ($scope, $location, Token, Auth) {
+  $scope.doLogin = function () {
+    var token = new Token($scope.credentials);
+    token.$save(function (response) {
+      if (response.token) {
+        // Set up auth service
+        Auth.setUser(response.token);
+
+        // Redirect
+        $location.path('/');
+      }
+    }, function (err) {
+        alert('Unable to log in - please check your details are correct');
+    });
+  };
+});
---
+++ test/controllers.spec.js
@@ -0,0 +1,63 @@
+'use strict';
+
+describe('Controllers', function () {
+
+  beforeEach(function(){
+    jasmine.addMatchers({
+      toEqualData: function(util, customEqualityTesters) {
+        return {
+          compare: function(actual, expected) {
+            return {
+              pass: angular.equals(actual, expected)
+            };
+          }
+        };
+      }
+    });
+  });
+
+  beforeEach(angular.mock.module('mynewanimalfriend.controllers'));
+
+  describe('Login Controller', function () {
+    var mockBackend, scope;
+
+    beforeEach(inject(function ($rootScope, $controller, _$httpBackend_) {
+      mockBackend = _$httpBackend_;
+      scope = $rootScope.$new();
+      $controller('LoginCtrl', {
+        $scope: scope
+      });
+    }));
+
+    // Test controller scope is defined
+    it('should define the scope', function () {
+      expect(scope).toBeDefined();
+    });
+
+    // Test doLogin is defined
+    it('should define the login method', function () {
+      expect(scope.doLogin).toBeDefined();
+    });
+
+    // Test doLogin works
+    it('should allow the user to log in', function () {
+      // Mock the backend
+      mockBackend.expectPOST('/api/authenticate', '{"email":"user@example.com","password":"password"}').respond({token: 123});
+
+      // Define login data
+      scope.credentials = {
+        email: 'user@example.com',
+        password: 'password'
+      };
+
+      //  Submit the request
+      scope.doLogin();
+
+      // Flush the backend
+      mockBackend.flush();
+
+      // Check login complete
+      expect(localStorage.getItem('authHeader')).toEqual('Bearer 123');
+    });
+  });
+});
